5b109c5be7d9e4b00923a50ee8ebcf8d3271c98d
cotide <xcli.code@gmail.com>
2019-08-01 11:51:36 +0800

    👶 update to sql2o-plus 1.0.3-SNAPSHOT
    新增where or 的支持

---
+++ src/main/java/io/github/cotide/dapper/query/SqlOrClause.java
@@ -0,0 +1,11 @@
+package io.github.cotide.dapper.query;
+
+public class SqlWhereClause extends BaseClause {
+
+    public SqlWhereClause(Sql sql) {
+        super(sql);
+    }
+
+
+    
+}
---
+++ src/main/java/io/github/cotide/dapper/query/SqlWhereClause.java
@@ -0,0 +1,4 @@
+package io.github.cotide.dapper.query;
+
+public class SqlWhereClause {
+}
